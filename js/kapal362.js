01d6a28cac3205c9d13c12ea281b7e1e77781ffc
AdityaKapal362 <adityakapal362@gmail.com>
2024-12-16 23:12:25 +0700

    Update kapal362.js

--- js/kapal362.js
+++ js/kapal362.js
@@ -1,21 +1,26 @@
-  const toolbar = document.getElementById("toolbar");
-  const myBioSection = document.querySelector(".mybio");
-  window.addEventListener("scroll", () => {
-    const myBioSectionTop = myBioSection.getBoundingClientRect().bottom;
-    if (myBioSectionTop <= 0) {
-      toolbar.classList.remove("hidden");
-    } else {
-      toolbar.classList.add("hidden");
-    }
-  });
+const toolbar = document.getElementById("toolbar");
+const myBioSection = document.querySelector(".mybio");
+const menuIcon = document.querySelector(".toolbar-menu-icon");
+const dropdownMenu = document.querySelector(".dropdown-menu");
 
-  const menuIcon = document.querySelector(".toolbar-menu-icon");
-  const dropdownMenu = document.querySelector(".dropdown-menu");
-  menuIcon.addEventListener("click", () => {
-    dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
-  });
-  document.addEventListener("click", (event) => {
-    if (!menuIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
-      dropdownMenu.style.display = "none";
-    }
-  });
+window.addEventListener("scroll", () => {
+  const myBioSectionTop = myBioSection.getBoundingClientRect().bottom;
+  if (myBioSectionTop <= 0) {
+    toolbar.classList.remove("hidden");
+  } else {
+    toolbar.classList.add("hidden");
+  }
+});
+
+menuIcon.addEventListener("click", () => {
+  const isMenuOpen = dropdownMenu.style.display === "block";
+  dropdownMenu.style.display = isMenuOpen ? "none" : "block";
+  menuIcon.src = isMenuOpen ? "/mine/menu-icon.png" : "/mine/close-icon.png";
+});
+
+document.addEventListener("click", (event) => {
+  if (!menuIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
+    dropdownMenu.style.display = "none";
+    menuIcon.src = "/mine/menu-icon.png";
+  }
+});
