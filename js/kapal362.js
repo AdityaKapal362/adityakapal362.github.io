e5c01353510af704427487f5345a0431a00cd8b2
AdityaKapal362 <adityakapal362@gmail.com>
2024-12-20 20:58:11 +0700

    Update kapal362.js

--- js/kapal362.js
+++ js/kapal362.js
@@ -13,14 +13,14 @@ window.addEventListener("scroll", () => {
 });
 
 menuIcon.addEventListener("click", () => {
-  const isMenuOpen = dropdownMenu.style.display === "block";
-  dropdownMenu.style.display = isMenuOpen ? "none" : "block";
-  menuIcon.src = isMenuOpen ? "/mine/menu-icon.png" : "/mine/close-icon.png";
+  const isOpen = dropdownMenu.classList.contains("open");
+  dropdownMenu.classList.toggle("open", !isOpen);
+  menuIcon.src = isOpen ? "/mine/menu-icon.png" : "/mine/close-icon.png";
 });
 
 document.addEventListener("click", (event) => {
   if (!menuIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
-    dropdownMenu.style.display = "none";
+    dropdownMenu.classList.remove("open");
     menuIcon.src = "/mine/menu-icon.png";
   }
 });
