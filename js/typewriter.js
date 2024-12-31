c192638e5833242402536ffe98ed6e7fb5962c77
AdityaKapal362 <adityakapal362@gmail.com>
2024-12-31 13:21:31 +0000

    asyuuuu

---
+++ js/typewriter.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed(".typewriter", {
+      strings: [
+        "Aditya Putra Ramadhan,",
+        'a <u style="color: #0ef">Java</u> addict,',
+        "a CSS Nerd,",
+        '<u style="color: #ff0000">Indonesian</u>'
+      ],
+      typeSpeed: 30,
+      startDelay: 1500,
+      backSpeed: 20,
+      backDelay: 1000,
+      loop: true,
+      showCursor: false,
+      contentType: "html"
+    });
+  });
